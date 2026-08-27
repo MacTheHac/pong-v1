099e7f305d03b1e5b6b1ad74682a7fa67ffbc82d
Markus Hacker <markus.hacker@ezsh.de>
2026-08-27 09:17:32 +0200

    Playersteuerung auf Maus umgestellt

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,8 @@
 extends CharacterBody2D
 
-const SPEED := 520.0
 const LEFT_LIMIT := 150.0
 const RIGHT_LIMIT := 810.0
 
 func _physics_process(_delta: float) -> void:
-	velocity.x = 0.0
-	velocity.y = 0.0
-
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -SPEED
-
-	if Input.is_action_pressed("move_right"):
-		velocity.x = SPEED
-
-	move_and_slide()
-	position.x = clamp(position.x, LEFT_LIMIT, RIGHT_LIMIT)
+	var mouse_position := get_global_mouse_position()
+	position.x = clamp(mouse_position.x, LEFT_LIMIT, RIGHT_LIMIT)
